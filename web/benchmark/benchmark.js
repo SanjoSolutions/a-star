80e495ec747f08c2b98d35cceb0d238594971793
Andriod <andre.ok@online.no>
2025-07-13 23:57:02 +0200

    Benchmark more precise locally

--- web/benchmark/benchmark.js
+++ web/benchmark/benchmark.js
@@ -12,22 +12,24 @@ $(function() {
             start = graph.grid[0][0],
             end = graph.grid[140][140],
             results = [],
-            times = 0;
+            startTime = performance.now(),
+            iterations = 1000;
 
-        for (var i = 0; i < 1000; i++) {
-            var startTime = performance ? performance.now() : new Date().getTime(),
+        for (var i = 0; i < iterations; i++) {
+            var iterationStartTime = performance.now(),
                 result = AStar.search(graph, start, end),
-                endTime = performance ? performance.now() : new Date().getTime();
-            times = times + (endTime - startTime);
+                iterationEndTime = performance.now();
 
             results.push(
                 '<li>Found path with ' + result.length + ' steps.  ' +
-                'Took ' + (endTime - startTime).toFixed(2) + ' milliseconds.</li>'
+                'Took ' + (iterationEndTime - iterationStartTime).toFixed(2) + ' milliseconds.</li>'
             );
         }
 
+        var endTime = performance.now()
+
         $("#graph").html(graph.toString());
-        $("#summary").html('Average time: ' + (times / 1000).toFixed(2) + 'ms');
+        $("#summary").html('Average time: ' + ((endTime - startTime) / iterations).toFixed(2) + 'ms');
         $("#results").html(results.join(''));
 
         running = false;
